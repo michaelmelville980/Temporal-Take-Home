fd3950b0499a575dcb11c4011fa295d85d0737c5
michael1303 <mmelville980@gmail.com>
2025-08-20 16:48:59 -0400

    feat(db): add column information to orders table

--- src/db/models.py
+++ src/db/models.py
@@ -1,16 +1,17 @@
-from sqlalchemy import Column, Integer, String, DateTime, func
+from sqlalchemy import Column, Integer, String, DateTime, func, JSONB
 from sqlalchemy.orm import declarative_base
+
 
 Base = declarative_base()
 metadata = Base.metadata
 
 class Orders(Base):
     __tablename__ = "orders"
-    id = Column() #Added via order_received function (order_id)
-    state = Column() #received, validated,
-    address_json = Column()
-    created_at = Column()
-    updated_at = Column()
+    id = Column(String(64), primary_key=True)
+    state = Column(String(30), nullable=False, server_default="received")
+    address_json = Column(JSONB, nullable=True)
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
 
 class Payments(Base):
     __tablename__ = "payments"
