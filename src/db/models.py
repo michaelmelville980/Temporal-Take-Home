a0235aa2eb8a522b980290a602ea5dc21a1fa1ff
michael1303 <mmelville980@gmail.com>
2025-08-20 17:08:06 -0400

    feat(db): add column information to payments table

--- src/db/models.py
+++ src/db/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, func, JSONB
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime, func, JSONB, ForeignKey, Numeric
+from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
@@ -8,18 +8,18 @@ metadata = Base.metadata
 class Orders(Base):
     __tablename__ = "orders"
     id = Column(String(64), primary_key=True)
-    state = Column(String(30), nullable=False, server_default="received")
+    state = Column(String(30), nullable=False, server_default="received") 
     address_json = Column(JSONB, nullable=True)
     created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
     updated_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now(), onupdate=func.now())
 
 class Payments(Base):
     __tablename__ = "payments"
-    payment_id = Column()
-    order_id = Column()
-    status = Column()
-    amount = Column()
-    created_at = Column()
+    payment_id = Column(String(64), primary_key=True)
+    order_id = Column(String, ForeignKey("orders.id", ondelete="CASCADE"), nullable=False)
+    status = Column(String(30), nullable=False)
+    amount = Column(Numeric(10, 2), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
 class Events(Base):
     __tablename__ = "events"
