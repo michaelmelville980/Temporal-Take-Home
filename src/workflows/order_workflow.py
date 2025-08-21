ab49aee93dcc04df38eb1f356eb6f51563dd1966
michael1303 <mmelville980@gmail.com>
2025-08-21 10:43:55 -0400

    feat(workflows): scaffold basic structure for order workflow

---
+++ src/workflows/order_workflow.py
@@ -0,0 +1,33 @@
+from temporalio import workflow
+from datetime import timedelta
+from activities import ReceiveOrder, ValidateOrder, ChargePayment
+from . import ShippingWorkflow
+from typing import Dict, Any, List
+
+@workflow.defn
+class OrderWorkflow:
+    @workflow.run
+    async def run (self, order_id: str, payment_id: str, items: List[Dict[str, Any]]):
+        
+        # Step 1: ReceiveOrder
+        await workflow.execute_activity(ReceiveOrder, order_id, items, schedule_to_close_timeout=timedelta(seconds=300))
+
+        # Step 2: ValidateOrder
+        await workflow.execute_activity(ValidateOrder, order_id, schedule_to_close_timeout=timedelta(seconds=300))
+
+        # Step 3: Timer (Manual Review)
+        # TODO
+
+        # Step 4: ChargePayment
+        await workflow.execute_activity(ChargePayment, order_id, payment_id, schedule_to_close_timeout=timedelta(seconds=300))
+
+        # Step 5: ShippingWorkflow
+        await workflow.execute_child_workflow(ShippingWorkflow.run, args=[order_id], task_queue="shipping-tq")
+
+
+
+   
+        
+
+                   
+      
