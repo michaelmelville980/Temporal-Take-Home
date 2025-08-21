9aaeedfff1c05c3f5bce0bd3ba667beae8a03656
michael1303 <mmelville980@gmail.com>
2025-08-21 15:15:47 -0400

    feat(db): create events when various operations called

--- src/db/crud.py
+++ src/db/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from . import models
 from typing import Dict, Any, List
+from decimal import Decimal
 
 def create_event(db: Session, order_id: str, type: str, payload_json: dict = None):
     event = models.Events(order_id=order_id, type=type, payload_json=payload_json)
@@ -12,21 +13,24 @@ def create_event(db: Session, order_id: str, type: str, payload_json: dict = Non
 def create_order(db: Session, order_id: str, items: List[Dict[str, Any]], address_json: Dict[str, Any]):
     order = db.query(models.Orders).filter_by(id=order_id).one_or_none()
     if order:
+        create_event(db, order_id, "create_order", {"message": "repeat request"})
         return order  
     order = models.Orders(id=order_id, items=items, address_json=address_json)
     db.add(order)
     db.commit()
     db.refresh(order)
-    create_event(db, order.id, "order_received", {"items": items, "address": address_json})
+    create_event(db, order_id, "create_order", {"message": "first request"})
     return order
 
 def validate_order(db: Session, order_id: str):
     order = db.query(models.Orders).filter_by(id=order_id).one_or_none()
-    if order.items and len(order.items) > 0:
+    if order is None:
+        create_event(db, order_id, "validate_order", {"message": "error, no order found"})
+    else:
         order.state = "validated"
         db.commit()
         db.refresh(order)
-        create_event(db, order.id, "order_validated", {"state": order.state})
+        create_event(db, order_id, "validate_order", {"message": "success"})
     return order
 
 def charge_payment(db: Session, order_id: str, payment_id: str):
@@ -36,44 +40,55 @@ def charge_payment(db: Session, order_id: str, payment_id: str):
             payment.status="charged"
             db.commit()
             db.refresh(payment)
+            create_event(db, order_id, "charge_payment", {"message": "success, payment status changed from failed to charged"})
+        if payment.status =="charged":
+             create_event(db, order_id, "charge_payment", {"message": "customer already charged"})
         return payment
     order = db.query(models.Orders).filter_by(id=order_id).one_or_none()
     if not order:
+        create_event(db, order_id, "charge_payment", {"message": "error, no order found"})
         raise ValueError(f"Order {order_id} not found")
-    cost = 0.0
+    cost = Decimal("0.00")
     for item in order.items:
-        cost += item["qty"] * item["price"]
+        cost += item["qty"] * Decimal(str(item["price"]))
     payment = models.Payments(payment_id = payment_id, order_id=order_id, status="charged", amount=cost)
     db.add(payment)
     db.commit()
     db.refresh(payment)
+    create_event(db, order_id, "charge_payment", {"message": "first payment successful"})
     return payment
 
 def prepare_package(db: Session, order_id: str):
     order = db.query(models.Orders).filter_by(id=order_id).one_or_none()
+    if order is None:
+        create_event(db, order_id, "prepare_package", {"message": "error, no order found"})
     if order.items and len(order.items) > 0:
         order.state = "package_prepared"
         db.commit()
         db.refresh(order)
-        create_event(db, order.id, "package_prepared", {"state": order.state})
+        create_event(db, order_id, "prepare_package", {"message": "success, package prepared"})
     return order
 
 def dispatch_carrier(db: Session, order_id: str):
     order = db.query(models.Orders).filter_by(id=order_id).one_or_none()
+    if order is None:
+        create_event(db, order_id, "dispatch_carrier", {"message": "error, no order found"})
     if order.items and len(order.items) > 0:
         order.state = "carrier_dispatched"
         db.commit()
         db.refresh(order)
-        create_event(db, order.id, "carrier_dispatched", {"state": order.state})
+        create_event(db, order_id, "dispatch_carrier", {"message": "success, carrier dispatched"})
     return order
 
 def ship_order(db: Session, order_id: str):
     order = db.query(models.Orders).filter_by(id=order_id).one_or_none()
+    if order is None:
+        create_event(db, order_id, "ship_order", {"message": "error, no order found"})
     if order.items and len(order.items) > 0:
         order.state = "shipped"
         db.commit()
         db.refresh(order)
-        create_event(db, order.id, "shipped", {"state": order.state})
+        create_event(db, order_id, "ship_order", {"message": "success, order shipped"})
     return order
 
     
