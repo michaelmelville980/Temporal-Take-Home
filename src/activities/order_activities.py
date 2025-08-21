308201b4317fb5dee512bb55536bed9635e5e01b
michael1303 <mmelville980@gmail.com>
2025-08-21 16:25:01 -0400

    fix(activities): makes activities async/await, fixes imports, and returns

--- src/activities/order_activities.py
+++ src/activities/order_activities.py
@@ -1,18 +1,18 @@
-from teporalio import activity
+from temporalio import activity
 from typing import Dict, Any, List
-from . import order_received, order_validated, payment_charged
+from services.order_service import order_received, order_validated, payment_charged
 
 @activity.defn
-async def ReceiveOrder(order_id: str, items: List[Dict[str, Any]]):
-    order_received(order_id, items)
+async def ReceiveOrder(order_id: str, items: List[Dict[str, Any]], address_json: Dict[str, Any]) -> Dict[str, Any]:
+    return await order_received(order_id, items, address_json)
 
 @activity.defn
-async def ValidateOrder(order_id: str):
-    order_validated(order_id)
+async def ValidateOrder(order_id: str) -> bool:
+    return await order_validated(order_id)
 
 @activity.defn
-async def ChargePayment(order_id: str, payment_id: str):
-    payment_charged(order_id, payment_id)
+async def ChargePayment(order_id: str, payment_id: str) -> Dict[str, Any]:
+    return await payment_charged(order_id, payment_id)
 
 
     
--- src/activities/shipping_activities.py
+++ src/activities/shipping_activities.py
@@ -1,18 +1,18 @@
-from teporalio import activity
+from temporalio import activity
 from typing import Dict, Any, List
-from . import package_prepared, carrier_dispatched, order_shipped
+from services.shipping_service import package_prepared, carrier_dispatched, order_shipped
 
 @activity.defn
-async def PreparePackage(order_id: str):
-    package_prepared(order_id)
+async def PreparePackage(order_id: str) -> str:
+    return await package_prepared(order_id)
 
 @activity.defn
-async def DispatchCarrier(order_id: str):
-    carrier_dispatched(order_id)
+async def DispatchCarrier(order_id: str) -> str:
+    return await carrier_dispatched(order_id)
 
 @activity.defn
-async def ShipOrder(order_id: str):
-    order_shipped(order_id)
+async def ShipOrder(order_id: str) -> str:
+    return await order_shipped(order_id)
 
 
     
