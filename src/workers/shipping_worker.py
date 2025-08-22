2273d8cb6af44027ae2cb0c44b87da3ecf37ca6e
michael1303 <mmelville980@gmail.com>
2025-08-22 02:51:46 -0400

    fix(workers): fixes bug where OrderWorkflow was called

--- src/workers/shipping_worker.py
+++ src/workers/shipping_worker.py
@@ -1,16 +1,18 @@
 import asyncio
 from temporalio.client import Client
 from temporalio.worker import Worker
-from workflows.order_workflow import OrderWorkflow
+from workflows.shipping_workflow import ShippingWorkflow
 from activities.shipping_activities import PreparePackage, DispatchCarrier
 
+
 async def main():
+
     client = await Client.connect("localhost:7233")
 
     worker = Worker(
         client,
         task_queue="shipping-tq",
-        workflows=[OrderWorkflow],
+        workflows=[ShippingWorkflow],
         activities=[PreparePackage, DispatchCarrier]
     )
     await worker.run()
