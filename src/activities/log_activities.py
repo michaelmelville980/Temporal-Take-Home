36f20cb6987c0501814646a1e1a5a0a0cb27b182
michael1303 <mmelville980@gmail.com>
2025-08-22 15:29:14 -0400

    refactor(activities): removes logger activity and instead calls persist_event_log before and after other activities
    Simplifies workflows by not adding logging as a step in the process.

--- src/activities/log_activities.py
+++
@@ -1,8 +0,0 @@
-from temporalio import activity
-from services.event_service import persist_event_log 
-
-@activity.defn
-async def CreateEventLog(order_id: str, event_type: str, payload_json: dict):
-    return persist_event_log(order_id=order_id, event_type=event_type, payload_json=payload_json)
-
-
