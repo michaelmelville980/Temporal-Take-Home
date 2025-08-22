2f9b8f0ec9477109266f458d1a271c76af579f9f
michael1303 <mmelville980@gmail.com>
2025-08-21 20:02:08 -0400

    feat(workers): add activities to workers

--- src/workers/order_worker.py
+++ src/workers/order_worker.py
@@ -2,6 +2,8 @@ import asyncio
 from temporalio.client import Client
 from temporalio.worker import Worker
 from workflows.order_workflow import OrderWorkflow
+from activities.order_activities import ReceiveOrder, ValidateOrder, ChargePayment
+from activities.signal_activities import CancelOrder, UpdateAddress
 
 async def main():
     client = await Client.connect("localhost:7233")
@@ -10,8 +12,8 @@ async def main():
         client,
         task_queue="order-tq",
         workflows=[OrderWorkflow],
+        activities=[ReceiveOrder, ValidateOrder, ChargePayment, CancelOrder, UpdateAddress]
     )
-
     await worker.run()
 
 if __name__ == "__main__":
--- src/workers/shipping_worker.py
+++ src/workers/shipping_worker.py
@@ -2,6 +2,7 @@ import asyncio
 from temporalio.client import Client
 from temporalio.worker import Worker
 from workflows.order_workflow import OrderWorkflow
+from activities.shipping_activities import PreparePackage, DispatchCarrier
 
 async def main():
     client = await Client.connect("localhost:7233")
@@ -10,8 +11,8 @@ async def main():
         client,
         task_queue="shipping-tq",
         workflows=[OrderWorkflow],
+        activities=[PreparePackage, DispatchCarrier]
     )
-
     await worker.run()
 
 if __name__ == "__main__":
