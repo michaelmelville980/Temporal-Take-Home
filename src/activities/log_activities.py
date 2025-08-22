6073d3ff00c4627c1fc8c800851ad530103d329b
michael1303 <mmelville980@gmail.com>
2025-08-22 14:11:49 -0400

    refactor(activities): Changes log_activities to match other activity naming conventions

--- src/activities/log_activities.py
+++ src/activities/log_activities.py
@@ -2,7 +2,7 @@ from temporalio import activity
 from services.event_service import persist_event_log 
 
 @activity.defn
-async def create_event_log(order_id: str, event_type: str, payload_json: dict):
+async def CreateEventLog(order_id: str, event_type: str, payload_json: dict):
     return persist_event_log(order_id=order_id, event_type=event_type, payload_json=payload_json)
 
 
