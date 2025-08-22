f1f2b2b76a0829236cbf56dbd823f3a7bbf136a9
michael1303 <mmelville980@gmail.com>
2025-08-22 02:52:51 -0400

    fix(workflows): fixes shpping workflow args specification

--- src/workflows/shipping_workflow.py
+++ src/workflows/shipping_workflow.py
@@ -1,19 +1,22 @@
 from temporalio import workflow
 from datetime import timedelta
 from activities.shipping_activities import PreparePackage, DispatchCarrier
-from workflows.order_workflow import OrderWorkflow
 from typing import Dict, Any, List
 import asyncio 
 
 @workflow.defn
 class ShippingWorkflow:
     @workflow.run
-    async def run (self, order_id: str, parent_id: str):
+    async def run(self, order_id: str, parent_id: str):
         parent = workflow.get_external_workflow_handle(parent_id)
         
         # Step 1: PreparePackage
         try:
-            await workflow.execute_activity(PreparePackage, order_id, schedule_to_close_timeout=timedelta(seconds=300))
+            await workflow.execute_activity(
+                PreparePackage,
+                args=[order_id],
+                schedule_to_close_timeout=timedelta(seconds=300),
+            )
         except asyncio.CancelledError:
             raise
         except Exception as e:
@@ -22,11 +25,16 @@ class ShippingWorkflow:
 
         # Step 2: DispatchCarrier
         try:
-            await workflow.execute_activity(DispatchCarrier, order_id, schedule_to_close_timeout=timedelta(seconds=300))
+            await workflow.execute_activity(
+                DispatchCarrier,
+                args=[order_id],
+                schedule_to_close_timeout=timedelta(seconds=300),
+            )
+            return "done"
         except asyncio.CancelledError:
             raise
         except Exception as e:
             await parent.signal("dispatch_failed", "DispatchCarrier failed")
             raise
-
-        return
+        
+   
